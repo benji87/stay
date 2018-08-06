75a9581e1d2079d5409e0c2fa9e0987b98abac6f
Ben Gordon <ben@mypfc.com>
2018-08-06 15:44:32 +0100

    Removed files

--- constants.js
+++
@@ -1,7 +0,0 @@
-const WEBSOCKET_URL = 'ws://192.168.1.18/api/websocket';
-const WEBSOCKET_PASS = '6adNt7XuzeRt35nc';
-const MONGODB_URL = 'mongodb://localhost:27017/stay';
-const API_VERSION = 'v1';
-const PROJECT_NAME = 'stay';
-
-module.exports = { WEBSOCKET_URL, WEBSOCKET_PASS, MONGODB_URL };
